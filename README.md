0b50b464c5e351c97c34baebcc57b3c9a5646787
unknown <msio94@hoymail.com>
2023-01-19 21:58:24 -0300

    finished

---
+++ README.md
@@ -0,0 +1,3 @@
+Hi, my name is Maximiliano Sio. I´m 28 years old and I am an industrial engineer.
+
+This is my first project which I am doing with the digital house course.
---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+//app.use(express.static(path.resolve(__dirname, "./public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/home.html"))
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req, res) => {
+  res.sendFile(__dirname + '/views/register.html');
+});
+
+app.listen(PORT, () => {
+  console.log(`server listening on ${PORT}`);
+});
