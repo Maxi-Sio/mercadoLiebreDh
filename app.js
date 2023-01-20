5bdd24b39605d826da1f1b5b401e8726a9e9a3a2
unknown <msio94@hoymail.com>
2023-01-19 22:14:04 -0300

    changes

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ app.get("/", (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-  res.sendFile(__dirname + '/views/login.html');
+  res.sendFile(__dirname + './views/login.html');
 });
 
 app.get('/register', (req, res) => {
-  res.sendFile(__dirname + '/views/register.html');
+  res.sendFile(__dirname + './views/register.html');
 });
 
 app.listen(PORT, () => {
